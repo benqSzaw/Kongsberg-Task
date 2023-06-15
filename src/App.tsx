4d0e9feac286026fe7d138682180b786ffd8e653
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-06-15 19:05:13 +0200

    add(Table)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "./components/Form/Form";
+import Table from "./components/Table/Table";
 
 const App = () => {
   return (
     <div>
       <Form />
+      <Table />
     </div>
   );
 };
