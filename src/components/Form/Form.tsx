38bd63038229abeb4fd7e0b7ee36a1515d84150d
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-06-15 13:33:59 +0200

    add(Form)

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,13 @@
+import "./form.sass";
+
+const Forn = () => {
+  return (
+    <div>
+      <label htmlFor="title">Title: </label>
+      <input type="text" id="title" />
+      <button> Get books </button>
+    </div>
+  );
+};
+
+export default Forn;
