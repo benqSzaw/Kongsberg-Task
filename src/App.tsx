2ad84b5b7d20ad36da644d400b2ca0ce852459a2
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-06-16 09:59:16 +0200

    style(App,Form,Table)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.scss"
 import Form from "./components/Form/Form";
 import Table from "./components/Table/Table";
 
 const App = () => {
   return (
-    <div>
+    <div className="main-container">
       <Form />
       <Table />
     </div>
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,11 +1,22 @@
 import "./table.scss";
 
 const Table = () => {
-    return <div className="table-container">
-        <table>
-
-        </table>
-    </div>;
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>a</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>b</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
