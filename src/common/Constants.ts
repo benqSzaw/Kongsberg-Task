74054540cef196a8ea83357fd42c7b959ed26575
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-06-17 12:34:28 +0200

    refactor(Constants)

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -2,6 +2,5 @@ export const apiUrl = "https://openlibrary.org/search.json?";
 const apiImageUrl = "https://covers.openlibrary.org/b/id/*.jpg";
 
 export const getImageUrl = (id: string, size: "S" | "M"): string => {
-  const url = apiImageUrl.replace("*", `${id}-${size}`);
-  return url;
+  return apiImageUrl.replace("*", `${id}-${size}`);
 };
