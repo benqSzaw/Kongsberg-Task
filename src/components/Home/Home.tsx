3696e7196aef4ea3d61a46377688a3133a973561
benqSzaw <45177027+benqSzaw@users.noreply.github.com>
2023-06-20 19:18:44 +0200

    format(Home)

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,40 +1,40 @@
 import { apiUrl } from "../../common/Constants";
 import "./home.scss";
 import {
-    SiRedux,
-    SiReact,
-    SiTypescript,
-    SiBootstrap,
-    SiAxios,
-    SiSass,
-    SiPrettier,
-    SiEslint,
+  SiRedux,
+  SiReact,
+  SiTypescript,
+  SiBootstrap,
+  SiAxios,
+  SiSass,
+  SiPrettier,
+  SiEslint,
 } from "react-icons/si";
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <div className="technologies">
-                <span>Used technologies:</span>
-                <div className="logos">
-                    <SiReact />
-                    <SiTypescript />
-                    <SiRedux />
-                    <SiBootstrap />
-                    <SiAxios />
-                    <SiSass />
-                    <SiPrettier />
-                    <SiEslint />
-                </div>
-            </div>
-            <div className="api">
-                <a href={apiUrl} target="_blank">
-                    Api from: {apiUrl}
-                </a>
-            </div>
-            <div className="author">Made by Beniamin Szawracki</div>
+  return (
+    <div className="home-container">
+      <div className="technologies">
+        <span>Used technologies:</span>
+        <div className="logos">
+          <SiReact />
+          <SiTypescript />
+          <SiRedux />
+          <SiBootstrap />
+          <SiAxios />
+          <SiSass />
+          <SiPrettier />
+          <SiEslint />
         </div>
-    );
+      </div>
+      <div className="api">
+        <a href={apiUrl} target="_blank">
+          Api from: {apiUrl}
+        </a>
+      </div>
+      <div className="author">Made by Beniamin Szawracki</div>
+    </div>
+  );
 };
 
 export default Home;
